39878fcdf255a8bcad90b682cf5d6032c21c9a25
Natasha Popenoe <npopenoe@ucsd.edu>
2024-07-31 15:51:03 -0700

    mutliply by 2 not 4 surface eqn

--- solid_angle.py
+++ solid_angle.py
@@ -15,7 +15,7 @@ def calculate_normal(point, mirror):
             point.y / np.sqrt(R**2 - (K + 1) * (point.x**2 + point.y**2)), 
             -1
         ])
-
+    
 def surface_primary(x, y, primary_radius_curv):
     return (x**2 + y**2) / (2 * primary_radius_curv)
 
@@ -32,11 +32,26 @@ def bounce_1(ray_direction, normal):
     normal = normal / np.linalg.norm(normal)
     return ray_direction - 2 * np.dot(ray_direction, normal) * normal
 
-def trace_ray(telescope, point):
-    # calc the direction of the ray to the primary mirror (vertical downward)
-    ray_direction = np.array([0, 0, -1])
+def circular_mask(X, Y, radius):
+    return np.sqrt(X**2 + Y**2) <= radius
+
+def generate_random_point_on_primary(X_primary, Y_primary, Z_primary, mask_primary):
+    # flatten the arrays and select only the valid points within the mask
+    valid_points = np.where(mask_primary)
+    idx = np.random.choice(len(valid_points[0]))
+    x = X_primary[valid_points][idx]
+    y = Y_primary[valid_points][idx]
+    z = Z_primary[valid_points][idx]
+    return Point(x, y, z)
+
+def trace_ray(telescope, point, X_primary, Y_primary, Z_primary, mask_primary):
+    mirror_point = generate_random_point_on_primary(X_primary, Y_primary, Z_primary, mask_primary)
+
+    # Calculate the direction from the particle to the random point on the primary mirror
+    ray_direction = np.array([mirror_point.x - point.x, mirror_point.y - point.y, mirror_point.z - point.z])
+    ray_direction /= np.linalg.norm(ray_direction)
     
-    # finds the intersection with the primary mirror
+ # finds the intersection with the primary mirror
     z_primary = surface_primary(point.x, point.y, telescope.primary.radius_curv)  # primary mirror surface
     t_primary = (z_primary - point.z) / ray_direction[2]  # calc the distance to the intersection
     target_primary = Point(
@@ -92,7 +107,7 @@ def plot_secondary_mirror(ax, telescope):
     # wireframe for the secondary mirror
     ax.plot_wireframe(X_secondary, Y_secondary, Z_secondary, color='purple', alpha=0.3)
 
-def visualize_rays(telescope, rays):
+def visualize_rays(telescope, rays, X_primary, Y_primary, Z_primary, mask_primary):
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection='3d')
     
@@ -102,53 +117,34 @@ def visualize_rays(telescope, rays):
 
         point, target_primary, target_secondary, reflected_secondary, normal_primary, normal_secondary = ray_path
 
-        # plot initial point
         ax.scatter(point.x, point.y, point.z, color='blue', marker='o', label='Atmospheric Particle')
-
-        # plot primary reflection
         ax.plot([point.x, target_primary.x], [point.y, target_primary.y], [point.z, target_primary.z], color='orange', label='Photon Ray')
         ax.plot([target_primary.x, target_secondary.x], [target_primary.y, target_secondary.y], [target_primary.z, target_secondary.z], color='steelblue', label='Primary Reflection')
 
-        '''     # plotting normal vector at the mirror intersection
-        ax.quiver(target_secondary.x, target_secondary.y, target_secondary.z, 
-                  normal_secondary[0], normal_secondary[1], normal_secondary[2], 
-                  length=1, color='red', normalize=True, arrow_length_ratio=0.1)
-        
-        ax.quiver(target_primary.x, target_primary.y, target_primary.z, 
-                  -normal_primary[0], -normal_primary[1], -normal_primary[2], 
-                  length=1, color='blue', normalize=True, arrow_length_ratio=0.1)'''
-
-        # plot the final line using the angle of reflection
         pink_line_length = 20.0 
         pink_endpoint = Point(target_secondary.x + pink_line_length * reflected_secondary[0],
                                target_secondary.y + pink_line_length * reflected_secondary[1],
                                target_secondary.z + pink_line_length * reflected_secondary[2])
         ax.plot([target_secondary.x, pink_endpoint.x], [target_secondary.y, pink_endpoint.y], [target_secondary.z, pink_endpoint.z], color='palevioletred', label='Secondary Reflection')
 
-    # mask for the circular mirrors
-    def circular_mask(X, Y, radius):
-        return np.sqrt(X**2 + Y**2) <= radius
-
-    # plot the mirrors
-    theta = np.linspace(0, 2 * np.pi, 100)
-    r_primary = 5.4745  # Actual radius of the primary mirror
-    x_primary = np.linspace(-r_primary, r_primary, 100)
-    y_primary = np.linspace(-r_primary, r_primary, 100)
-    X_primary, Y_primary = np.meshgrid(x_primary, y_primary)
-    mask_primary = circular_mask(X_primary, Y_primary, r_primary)
-    Z_primary = np.zeros_like(X_primary)
-    Z_primary[mask_primary] = (X_primary[mask_primary]**2 + Y_primary[mask_primary]**2) / (2 * telescope.primary.radius_curv)
-    Z_primary[~mask_primary] = np.nan
     ax.plot_wireframe(X_primary, Y_primary, Z_primary, color='green', alpha=0.3)
-    plot_secondary_mirror(ax, telescope)
 
-    '''# Plot focal points for reference
-    ax.scatter(0, 0, 17.5, color='green', marker='o', label='Primary Focal Point', alpha=0.3)
-    ax.scatter(0, 0, -2.5, color='purple', marker='o', label='Back Focal Dist', alpha=0.3) ''' 
+    r_secondary = 0.7090145
+    x_secondary = np.linspace(-r_secondary, r_secondary, 100)
+    y_secondary = np.linspace(-r_secondary, r_secondary, 100)
+    X_secondary, Y_secondary = np.meshgrid(x_secondary, y_secondary)
+    mask_secondary = circular_mask(X_secondary, Y_secondary, r_secondary)
+    Z_secondary = np.zeros_like(X_secondary)
+    Z_secondary[mask_secondary] = telescope.secondary.surface(X_secondary[mask_secondary], Y_secondary[mask_secondary])
+    Z_secondary[~mask_secondary] = np.nan
+    ax.plot_wireframe(X_secondary, Y_secondary, Z_secondary, color='purple', alpha=0.3)
 
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
+    ax.set_xlim([-6, 6])
+    ax.set_ylim([-6, 6])
+    ax.set_zlim([0, 20])
     ax.view_init(elev=30, azim=60)
     ax.set_box_aspect([1, 1, 1])
     plt.show()
@@ -159,9 +155,18 @@ if __name__ == "__main__":
     b = 2.5
     cassegrain_geo = CassegrainGeometry(F, b, f1)
     
-    # points across the diameter of the primary mirror spaced by 0.1
-    x_values = np.linspace(-5, 5, 11)
+    theta = np.linspace(0, 2 * np.pi, 100)
+    r_primary = 5.4745
+    x_primary = np.linspace(-r_primary, r_primary, 100)
+    y_primary = np.linspace(-r_primary, r_primary, 100)
+    X_primary, Y_primary = np.meshgrid(x_primary, y_primary)
+    mask_primary = circular_mask(X_primary, Y_primary, r_primary)
+    Z_primary = np.zeros_like(X_primary)
+    Z_primary[mask_primary] = (X_primary[mask_primary]**2 + Y_primary[mask_primary]**2) / (4 * cassegrain_geo.primary.radius_curv)
+    Z_primary[~mask_primary] = np.nan
+    
+    x_values = np.linspace(-5, 5, 1)
     test_points = [Point(x, 0, 20) for x in x_values]
 
-    rays = [trace_ray(cassegrain_geo, point) for point in test_points]
-    visualize_rays(cassegrain_geo, rays)
+    rays = [trace_ray(cassegrain_geo, point, X_primary, Y_primary, Z_primary, mask_primary) for point in test_points]
+    visualize_rays(cassegrain_geo, rays, X_primary, Y_primary, Z_primary, mask_primary)
